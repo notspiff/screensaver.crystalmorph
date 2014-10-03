9b77b58433a29ec2186c0ed868f93ef8273a38ce
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-10-03 13:11:05 +0200

    s

--- src/Fractal.h
+++ src/Fractal.h
@@ -1,30 +1,7 @@
 #pragma once
 // include file for screensaver template
 
-#include <xtl.h>
 
-
-extern "C" void d3dSetTextureStageState( int x, DWORD dwY, DWORD dwZ);
-extern "C" void d3dSetRenderState(DWORD dwY, DWORD dwZ);
-extern "C" void d3dGetRenderState(DWORD dwY, DWORD* dwZ);
-extern "C" void d3dSetTransform(DWORD dwY, D3DMATRIX* dwZ);
-extern "C" 
-{
-	struct SCR_INFO 
-	{
-		int	dummy;
-	};
-};
-
-void LoadSettings();
-
-/*
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW|D3DFVF_DIFFUSE)
-struct CUSTOMVERTEX
-{
-	FLOAT x, y, z, rhw; // The transformed position for the vertex.
-    DWORD color; // The vertex colour.
-};*/
 struct Fractal;
 struct FractalController;
 
