4da61656cef6a8e2b1944e3b7edd9fd8a9fabe91
Daniel Scheller <d.scheller@gmx.net>
2017-06-05 15:49:36 +0200

    rework to new OOP addon interface (fixes the addon after https://github.com/xbmc/xbmc/pull/12148)

--- src/Fractal.cpp
+++ src/Fractal.cpp
@@ -20,60 +20,91 @@
 */
 
 
-#include <xbmc_scr_dll.h>
+#include <kodi/addon-instance/Screensaver.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include "Fractal.h"
 #include "fractalcontroller.h"
 #include "Fractal.h"
 
-FractalSettings settings;
+// stuff for the background plane
 
-int	g_iWidth;
-int g_iHeight;
-float g_fRatio;
-
-void Step()
+struct BG_VERTEX
 {
-  if (settings.frame > settings.nextTransition)
-  {
-    settings.nextTransition = settings.frame + settings.transitionTime;
+    CVector position;
+    CRGBA color;
+};
 
-    if (settings.animationCountdown == 0)
+////////////////////////////////////////////////////////////////////////////////
+
+class CScreensaverCrystalmorph
+  : public kodi::addon::CAddonBase,
+    public kodi::addon::CInstanceScreensaver
+{
+  public:
+    CScreensaverCrystalmorph();
+
+    virtual bool Start() override;
+    virtual void Stop() override;
+    virtual void Render() override;
+
+  private:
+    FractalSettings m_settings;
+
+    int             m_iWidth;
+    int             m_iHeight;
+    float           m_fRatio;
+
+    CVector         m_lightDir = CVector(-0.5f, -0.5f, 0.5f);
+    BG_VERTEX       m_BGVertices[4];
+
+    void            Step();
+    void            CreateLight();
+    void            SetupGradientBackground(const CRGBA& dwTopColor, const CRGBA& dwBottomColor);
+    void            RenderGradientBackground();
+    void            SetCamera();
+    void            SetDefaults();
+};
+
+void CScreensaverCrystalmorph::Step()
+{
+  if (m_settings.frame > m_settings.nextTransition)
+  {
+    m_settings.nextTransition = m_settings.frame + m_settings.transitionTime;
+
+    if (m_settings.animationCountdown == 0)
     {
-      settings.fractalcontroller->SetAnimation(true);
-      settings.animationCountdown = 3+rand()%5;
-      settings.nextTransition = settings.frame + settings.animationTime;
+      m_settings.fractalcontroller->SetAnimation(true);
+      m_settings.animationCountdown = 3+rand()%5;
+      m_settings.nextTransition = m_settings.frame + m_settings.animationTime;
     }
     else
     {
-      settings.fractalcontroller->SetAnimation(false);
+      m_settings.fractalcontroller->SetAnimation(false);
 
-      if (settings.animationCountdown-- == 1)
+      if (m_settings.animationCountdown-- == 1)
       {
-        settings.fractalcontroller->SetToRandomFractal(3+rand()%3);
+        m_settings.fractalcontroller->SetToRandomFractal(3+rand()%3);
       }
       else
       {
         static int preset = rand()%6;
-        if (frand() < settings.presetChance)
-          settings.fractalcontroller->SetDesiredToPreset((preset+rand()%5)%6);
+        if (frand() < m_settings.presetChance)
+          m_settings.fractalcontroller->SetDesiredToPreset((preset+rand()%5)%6);
         else
         {
-          if (rand()%70 == 0) settings.fractalcontroller->SetToRandomFractal(8);
-          else settings.fractalcontroller->SetToRandomFractal();
+          if (rand()%70 == 0) m_settings.fractalcontroller->SetToRandomFractal(8);
+          else m_settings.fractalcontroller->SetToRandomFractal();
         }
       }
-      settings.fractalcontroller->StartMorph();
+      m_settings.fractalcontroller->StartMorph();
     }
   }
-  settings.fractalcontroller->UpdateFractalData();
-  settings.frame++;
+  m_settings.fractalcontroller->UpdateFractalData();
+  m_settings.frame++;
 }
 
-CVector g_lightDir = CVector(-0.5f, -0.5f, 0.5f);
-
-void CreateLight()
+void CScreensaverCrystalmorph::CreateLight()
 {
   const GLfloat amb[] = {0.5, 0.0, 0.0, 1.0};
   glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
@@ -83,7 +114,7 @@ void CreateLight()
   glLightfv(GL_LIGHT0, GL_SPECULAR, spec);
   const GLfloat pos[] = {5.0, -5.0, 10.0, 1.0};
   glLightfv(GL_LIGHT0, GL_POSITION, pos);
-  const GLfloat dir[] = {g_lightDir.x, g_lightDir.y, g_lightDir.z, 1.0};
+  const GLfloat dir[] = {m_lightDir.x, m_lightDir.y, m_lightDir.z, 1.0};
   glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, dir);
   float at = 0.5;
   glLightfv(GL_LIGHT0, GL_CONSTANT_ATTENUATION, &at);
@@ -99,111 +130,92 @@ void CreateLight()
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// stuff for the background plane
-
-struct BG_VERTEX 
-{
-    CVector position;
-    CRGBA color;
-};
-
-BG_VERTEX g_BGVertices[4];
-
-////////////////////////////////////////////////////////////////////////////////
-
 // fill in background vertex array with values that will
 // completely cover screen
-void SetupGradientBackground(const CRGBA& dwTopColor, const CRGBA& dwBottomColor)
+void CScreensaverCrystalmorph::SetupGradientBackground(const CRGBA& dwTopColor, const CRGBA& dwBottomColor)
 {
   float x1 = -0.5f;
   float y1 = -0.5f;
-  float x2 = (float)g_iWidth - 0.5f;
-  float y2 = (float)g_iHeight - 0.5f;
+  float x2 = (float)m_iWidth - 0.5f;
+  float y2 = (float)m_iHeight - 0.5f;
 
-  g_BGVertices[0].position = CVector( x2, y1, 0.0f);
-  g_BGVertices[0].color = dwTopColor;
+  m_BGVertices[0].position = CVector( x2, y1, 0.0f);
+  m_BGVertices[0].color = dwTopColor;
 
-  g_BGVertices[1].position = CVector( x2, y2, 0.0f);
-  g_BGVertices[1].color = dwBottomColor;
+  m_BGVertices[1].position = CVector( x2, y2, 0.0f);
+  m_BGVertices[1].color = dwBottomColor;
 
-  g_BGVertices[2].position = CVector( x1, y1, 0.0f);
-  g_BGVertices[2].color = dwTopColor;
+  m_BGVertices[2].position = CVector( x1, y1, 0.0f);
+  m_BGVertices[2].color = dwTopColor;
 
-  g_BGVertices[3].position = CVector( x1, y2, 0.0f);
-  g_BGVertices[3].color = dwBottomColor;
+  m_BGVertices[3].position = CVector( x1, y2, 0.0f);
+  m_BGVertices[3].color = dwBottomColor;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void RenderGradientBackground()
+void CScreensaverCrystalmorph::RenderGradientBackground()
 {
   glDisable(GL_TEXTURE_2D);
   glBegin(GL_TRIANGLE_STRIP);
   for (size_t i=0;i<4;++i)
   {
-    glColor3f(g_BGVertices[i].color.r, g_BGVertices[i].color.g,
-              g_BGVertices[i].color.b);
-    glVertex3f(g_BGVertices[i].position.x, g_BGVertices[i].position.y,
-               g_BGVertices[i].position.z);
+    glColor3f(m_BGVertices[i].color.r, m_BGVertices[i].color.g,
+              m_BGVertices[i].color.b);
+    glVertex3f(m_BGVertices[i].position.x, m_BGVertices[i].position.y,
+               m_BGVertices[i].position.z);
   }
   glEnd();
 }
 
-void SetCamera()
+void CScreensaverCrystalmorph::SetCamera()
 {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(45, g_fRatio, 0.1, 1000.0);
+  gluPerspective(45, m_fRatio, 0.1, 1000.0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   gluLookAt(0.0, 0.0, -2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
-void SetDefaults()
+void CScreensaverCrystalmorph::SetDefaults()
 {
-  settings.frame = 0;
-  settings.iMaxDepth = 5;
-  settings.iMaxObjects = 4000;
-  settings.nextTransition = 500;
-  settings.morphSpeed = 3.0;
-  settings.presetChance = 0.3;
-  settings.animationTime = 3000;
-  settings.transitionTime = 500;
+  m_settings.frame = 0;
+  m_settings.iMaxDepth = 5;
+  m_settings.iMaxObjects = 4000;
+  m_settings.nextTransition = 500;
+  m_settings.morphSpeed = 3.0;
+  m_settings.presetChance = 0.3;
+  m_settings.animationTime = 3000;
+  m_settings.transitionTime = 500;
 
-  settings.animationCountdown = 3+rand()%5;
+  m_settings.animationCountdown = 3+rand()%5;
 }
 
 ////////////////////////////////////////////////////////////////////////////
 // XBMC has loaded us into memory, we should set our core values
 // here and load any settings we may have from our config file
 //
-ADDON_STATUS ADDON_Create(void* hdl, void* props)
+CScreensaverCrystalmorph::CScreensaverCrystalmorph()
 {
-  if (!props)
-    return ADDON_STATUS_UNKNOWN;
-
-  SCR_PROPS* scrprops = (SCR_PROPS*)props;
-
-  g_iWidth = scrprops->width;
-  g_iHeight  = scrprops->height;
-  g_fRatio = (float)g_iWidth/(float)g_iHeight;
+  m_iWidth = Width();
+  m_iHeight = Height();
+  m_fRatio = (float)m_iWidth/(float)m_iHeight;
 
   SetCamera();
   SetDefaults();
-  settings.fractal = new Fractal();
-  settings.fractalcontroller = new FractalController(settings.fractal, &settings);
-  settings.fractalcontroller->SetMorphSpeed(settings.morphSpeed);
+  m_settings.fractal = new Fractal();
+  m_settings.fractalcontroller = new FractalController(m_settings.fractal, &m_settings);
+  m_settings.fractalcontroller->SetMorphSpeed(m_settings.morphSpeed);
   CreateLight();
-
-  return ADDON_STATUS_OK;
 }
 
 // XBMC tells us we should get ready
 // to start rendering. This function
 // is called once when the screensaver
 // is activated by XBMC.
-extern "C" void Start()
+bool CScreensaverCrystalmorph::Start()
 {
   SetupGradientBackground(CRGBA(0,0,0,255), CRGBA(0,60,60,255));
 }
@@ -213,94 +225,26 @@ extern "C" void Start()
 // each frame render in XBMC, you should
 // render a single frame only - the DX
 // device will already have been cleared.
-extern "C" void Render()
+void CScreensaverCrystalmorph::Render()
 {
   CreateLight();
   SetCamera();
   RenderGradientBackground();
   static int sx = (rand()%2)*2-1, sy = (rand()%2)*2-1;
 
-  glRotatef(sx*settings.frame*0.0051,1.0, 0.0, 0.0);
-  glRotatef(sy*settings.frame*0.0032,0.0, 1.0, 0.0);
+  glRotatef(sx*m_settings.frame*0.0051,1.0, 0.0, 0.0);
+  glRotatef(sy*m_settings.frame*0.0032,0.0, 1.0, 0.0);
   Step();
-  settings.fractal->Render();
+  m_settings.fractal->Render();
 }
 
 // XBMC tells us to stop the screensaver
 // we should free any memory and release
 // any resources we have created.
-extern "C" void ADDON_Stop()
+void CScreensaverCrystalmorph::Stop()
 {
-  delete settings.fractal;
-  delete settings.fractalcontroller;
+  delete m_settings.fractal;
+  delete m_settings.fractalcontroller;
 }
 
-extern "C" void ADDON_Destroy()
-{
-}
-
-extern "C" ADDON_STATUS ADDON_GetStatus()
-{
-  return ADDON_STATUS_OK;
-}
-
-extern "C" ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void *value)
-{
-  return ADDON_STATUS_OK;
-}
-
-// Load settings from the [screensavername].xml configuration file
-// the name of the screensaver (filename) is used as the name of
-// the xml file - this is sent to us by XBMC when the Init func
-// is called.
-//void LoadSettings()
-//{
-//        XmlNode node, childNode, grandChild;
-//        CXmlDocument doc;
-//        
-//         Set up the defaults
-//        SetDefaults();
-
-//        char szXMLFile[1024];
-//        strcpy(szXMLFile, "Q:\\screensavers\\");
-//        strcat(szXMLFile, g_szScrName);
-//        strcat(szXMLFile, ".xml");
-
-//        OutputDebugString("Loading XML: ");
-//        OutputDebugString(szXMLFile);
-
-//         Load the config file
-//        if (doc.Load(szXMLFile) >= 0)
-//        {
-//                node = doc.GetNextNode(XML_ROOT_NODE);
-//                while(node > 0)
-//                {
-//                        if (strcmpi(doc.GetNodeTag(node),"screensaver"))
-//                        {
-//                                node = doc.GetNextNode(node);
-//                                continue;
-//                        }
-//                        if (childNode = doc.GetChildNode(node,"maxobjects")){
-//        settings.iMaxObjects = atoi(doc.GetNodeText(childNode));
-//                        }
-//      if (childNode = doc.GetChildNode(node,"maxcutoffdepth")){
-//        settings.iMaxDepth = atoi(doc.GetNodeText(childNode));
-//                        }
-//      if (childNode = doc.GetChildNode(node,"morphspeed")){
-//        settings.morphSpeed = atof(doc.GetNodeText(childNode));
-//                        }
-//      if (childNode = doc.GetChildNode(node,"animationtime")){
-//        settings.animationTime = 60*atof(doc.GetNodeText(childNode));
-//                        }
-//      if (childNode = doc.GetChildNode(node,"transitiontime")){
-//        settings.nextTransition = settings.transitionTime = 60*atof(doc.GetNodeText(childNode));
-//                        }
-//      if (childNode = doc.GetChildNode(node,"presetchance")){
-//        settings.presetChance = atof(doc.GetNodeText(childNode));
-//                        }
-
-//                        node = doc.GetNextNode(node);
-//                }
-//                doc.Close();
-//        }
-//}
+ADDONCREATOR(CScreensaverCrystalmorph);
